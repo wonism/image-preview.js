02b353b2868fa5b76c14296be4e2c18394432f6c
Jaewon Kim <jaewonism57@gmail.com>
2016-06-01 00:02:33 +0900

    update files

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 # image-preview.js
 This is a open source project to get image preview when user upload image file.
 
-## How to use
-```html
-<!-- add easy-map.js in head -->
-<script type="text/javascript" src="path/to/image-preview.min.js"></script>
-```
-
 ## Browser Support
 - PC
   - Firefox(Gecko) : 3.6 (1.9.2)
@@ -20,60 +14,85 @@ This is a open source project to get image preview when user upload image file.
   - IE Mobile : 10
   - Opera Mobile : 11.5
   - Safari Mobile : 6.1
-- If you want to use in IE version, use polyfill for &lsquo;fileReader API&rsquo;.
+- If you want to use in IE version,
+  use polyfill for &lsquo;fileReader API&rsquo;.
 
-## Options (data-attributes)
-- `data-get-preview` - If you want to use this function, use this data-attribute in &lt;img&gt;, &lt;input&gt;.
-- `data-width` - Purpose for limit image size (width).
-- `data-height` - Purpose for limit image size (height).
-- `data-allow-type` - Purpose for limit image type. example) [type1, type2, type3]
-
-## Java Script parameters
-- first parameter : this (YOU MUST PASS **THIS** **HERE** !!!)
-- second parameter : function that launch after upload image successfully. It can get a object about uploaded file.
-- third parameter : function that launch after failed to upload image. It can get a object about uploaded file and &lsquo;what is wrong&rsquo;.
-
-## Example
+## How to use
 ```html
-<!-- Add <img>, <input> with 'data-get-preview' -->
-<div>
-  <input type="file" data-get-preview />
-  <img src="" alt="" data-get-preview' data-width="300"
-data-height="300" data-allow-type=[png, jpg, gif] />
-</div>
+<!-- your-html-file.html -->
+<!-- add image-preview.js in head -->
+<script type="text/javascript" src="path/to/image-preview.min.js"></script>
 ```
 
 ```js
-// You shoud bind the function to element.
+/***** your-js-file.js *****/
+imagePreview.init(ELEMENT, WIDTH, HEIGHT, RATIO, FILE_TYPE, FILE_SIZE,
+    CHOOSE_USING_BINARY_FILES, FUNCTION_THAT_EXECUTE_AFTER_SUCCESS,
+    FUNCTION_THAT_EXECUTE_AFTER_FAILED, FUNCTION_THAT_EXECUTE_AFTER_LOADED_IMAGES);
+```
 
+## Parameters
+- ELEMENT : Element **(REQUIRED !!)**
+  - Input elemenent
+- WIDTH : Number
+  - Width restriction (unit : px)
+- HEIGHT : Number
+  - Height restriction (unit : px)
+- RATIO : Number
+  - Constrain proportions
+- FILE_TYPE : String
+  - File type restriction
+- FILE_SIZE : Number or String with unit
+  - Maximum file size
+  - If use Number, unit is `bits`
+  - If use String, You can set unit like this... `50mb`, `20KB` (it ignores case)
+- CHOOSE_USING_BINARY_FILES : Boolean
+  - If you want to get binary files, You have to use TRUE.
+  - You can use binary files through 3rd parameter in
+    FUNCTION_THAT_EXECUTE_AFTER_SUCCESS
+- FUNCTION_THAT_EXECUTE_AFTER_SUCCESS : Function
+  - You can use 3 parameters
+    - 1. Right Files&rsquo; informations
+    - 2. Wrong Files&rsquo; informations
+    - 3. Binary Files that were from &gt;input&lt;
+- FUNCTION_THAT_EXECUTE_AFTER_FAILED : Function
+  - You can use 2 parameters
+    - 1. Right Files&rsquo; informations
+    - 2. Wrong Files&rsquo; informations
+- FUNCTION_THAT_EXECUTE_AFTER_FAILED : Function
+  - You can use a parameter
+    - A image (&#61; parameter) that was from input.
+    - It can be rendered by this function If you want.
+
+## Example
+```js
+var afterSuccess = function (obj, image, bins) {
+  console.log(obj, image, bins);
+};
+
+var afterFailed = function (obj, image) {
+  console.log(obj, image);
+};
+
+var renderFunc = function (image) {
+  console.log(image);
+  // Handle images...
+};
+
+/***** You shoud bind the function to element. *****/
 // Native Java Script
-var afterSuccess = function () {
-  console.log('Image is uploaded!');
+document.getElementById('file').onchange = function () {
+  // imagePreview.init(this, 0, 0, 0, 'png jpg', '10mb', true, afterSuccess, afterFailed, renderFunc);
+  imagePreview.init(this, 0, 0, 0, 'png jpg', '10mb', true, afterSuccess, afterFailed);
 };
-
-var afterFailed = function (obj) {
-  console.log(obj, 'Failed!');
-};
-
-var fileReaders = document.querySelectorAll('input[type="file"][data-get-preview]');
-
-for (var i = 0; i < fileReaders.length; i++) {
-  if (fileReaders[i].addEventListener) {
-    fileReaders[i].addEventListener('change', function() {
-      getPreview(this, afterSuccess, afterFailed);
-    });
-  } else if (fileReaders[i].attachEvent) {
-    fileReaders[i].attachEvent('onchange', function() {
-      getPreview(this, afterSuccess, afterFailed);
-    });
-  }
-}
 
 // jQuery
-$('body').on('change', 'input[type="file"][data-get-preview]') {
-  getPreview(this, afterSuccess, afterFailed);
+$('body').on('change', '#file', function () {
+  imagePreview.init(this, 0, 0, 0, 'png jpg', '10mb', true, afterSuccess, afterFailed);
 });
 ```
+
+## If you find **typo** or **incorrect expression** **Send a PULL REQUEST**
 
 ##Licence
 MIT
--- image-preview.js
+++ image-preview.js
@@ -2,7 +2,7 @@
  *
  * title   : Image Preview JS
  *
- * version : 0.1.0 (Beta)
+ * version : 0.2.0 (Beta)
  *
  * author  : Jaewon
  *
@@ -10,117 +10,253 @@
  *
  * github  : https://www.github.com/wonism
  *
- * blog : http://jaewonism.cf
+ * blog    : http://jaewonism.cf
  *
  */
 
-// Query Selector Polyfill
-if (!document.querySelectorAll) {
-  document.querySelectorAll = function (selectors) {
-    var style, elements, element;
+;(function () {
+  var imagePreview, ImagePreview;
 
-    style = document.createElement('style'), elements = [];
-    elements = [];
-    element = undefined;
+  // Variable for Singletone instance
+  var _imagePreviewInstance;
 
-    document.documentElement.firstChild.appendChild(style);
-    document._qsa = [];
+  imagePreview = {
+    init: function (element, width, height, ratio, type, volume, useBins, successFunc, failedFunc, renderFunc) {
+      if (arguments.length < 1) {
+        throw new Error('Invalid parameters !!!');
+      }
 
-    style.styleSheet.cssText = selectors + '{ x-qsa: expression(document._qsa && document._qsa.push(this)) }';
-    window.scrollBy(0, 0);
-    style.parentNode.removeChild(style);
-
-    while (document._qsa.length) {
-      element = document._qsa.shift();
-      element.style.removeAttribute('x-qsa');
-      elements.push(element);
-    }
-
-    document._qsa = null;
-    return elements;
+      return new ImagePreview(element, width, height, ratio, type, volume, useBins, successFunc, failedFunc, renderFunc);
+    },
+    version: "0.1.0"
   };
-}
 
-var getPreview = function (element, sfn, fFn) {
-  var image, width, height, reader;
+  ImagePreview = function (element, width, height, ratio, type, volume, useBins, successFunc, failedFunc, renderFunc) {
+    _imagePreviewInstance = this;
 
-  width = element.getAttribute('data-width') >> 0;
-  height = element.getAttribute('data-height') >> 0;
+    _imagePreviewInstance.obj = {};
 
-  reader = new FileReader();
+    _imagePreviewInstance.obj.notFittable = false;
+    _imagePreviewInstance.obj.wrongFiles = [];
+    _imagePreviewInstance.obj.rightFiles = [];
 
-  if (reader && element.files[0] && element.files[0].type.match(/image/)) {
-    var name, type, allowTypeArr, isCorrectType;
+    _imagePreviewInstance.obj.width = width >> 0;
+    _imagePreviewInstance.obj.height = height >> 0;
+    _imagePreviewInstance.obj.ratio = Number((ratio >> 0).toFixed(2));
+    _imagePreviewInstance.obj.type = type ? type.match(/jpg|jpeg|gif|png/) ? String(type) : '' : '';
+    _imagePreviewInstance.obj.volume = volume;
+    _imagePreviewInstance.obj.useBins = !!useBins;
+    _imagePreviewInstance.obj.bins = [];
+    _imagePreviewInstance.obj.successFunc = successFunc;
+    _imagePreviewInstance.obj.failedFunc = failedFunc;
+    _imagePreviewInstance.obj.renderFunc = renderFunc;
 
-    name = element.files[0].name;
-    type = element.files[0].type.replace(/image\//, '');
-    allowTypeArr = null;
-    isCorrectType = true;
+    _imagePreviewInstance.obj.acceptType = [];
+    _imagePreviewInstance.obj.useValidate = false;
+    _imagePreviewInstance.obj.validateType = [];
 
-    if (element.getAttribute('data-allow-type')) {
-      allowTypeArr = element.getAttribute('data-allow-type').replace(/^\[|\]$/g, '').split(',').map(function (str) { return str.trim(); });
-      if (type.match(/jpeg/)) {
-        if (allowTypeArr.indexOf(type) === -1 && allowTypeArr.indexOf('jpg') === -1) {
-          isCorrectType = false;
-        }
+    if (typeof _imagePreviewInstance.obj.volume === 'string' && _imagePreviewInstance.obj.volume.match(/^\d+/)) {
+      if (_imagePreviewInstance.obj.volume.match(/kb$/i)) {
+        _imagePreviewInstance.obj.volume = (_imagePreviewInstance.obj.volume.match(/\d+/)[0] >> 0) * 1024;
+      } else if (_imagePreviewInstance.obj.volume.match(/mb$/i)) {
+        _imagePreviewInstance.obj.volume = (_imagePreviewInstance.obj.volume.match(/\d+/)[0] >> 0) * 1024 * 1024;
+      } else if (_imagePreviewInstance.obj.volume.match(/gb$/i)) {
+        _imagePreviewInstance.obj.volume = (_imagePreviewInstance.obj.volume.match(/\d+/)[0] >> 0) * 1024 * 1024 * 1024;
       } else {
-        if (allowTypeArr.indexOf(type) === -1) {
-          isCorrectType = false;
-        }
+        _imagePreviewInstance.obj.volume = (_imagePreviewInstance.obj.volume.match(/\d+/)[0] >> 0); 
+      }
+    } else {
+      _imagePreviewInstance.obj.volume = _imagePreviewInstance.obj.volume >> 0;
+    }
+
+    if (_imagePreviewInstance.obj.ratio) {
+      _imagePreviewInstance.obj.useValidate = true;
+      _imagePreviewInstance.obj.validateType.push('ratio');
+    } else if (_imagePreviewInstance.obj.width || _imagePreviewInstance.obj.height) {
+      _imagePreviewInstance.obj.useValidate = true;
+      _imagePreviewInstance.obj.validateType.push('size');
+    }
+
+    if (_imagePreviewInstance.obj.volume) {
+      _imagePreviewInstance.obj.useValidate = true;
+      _imagePreviewInstance.obj.validateType.push('volume');
+    }
+
+    if (_imagePreviewInstance.obj.type) {
+      _imagePreviewInstance.obj.acceptType = _imagePreviewInstance.obj.type.match(/jpg|jpeg|gif|png/g);
+      _imagePreviewInstance.obj.validateType.push('type');
+
+      if (_imagePreviewInstance.obj.acceptType.includes('jpg') && !_imagePreviewInstance.obj.acceptType.includes('jpeg')) {
+        _imagePreviewInstance.obj.acceptType.push('jpeg');
       }
     }
 
-    reader.onload = function(e) {
-      image = document.createElement('img');
-      image.src = e.target.result;
-      image.onload = function() {
-        var w, h, obj;
-        w = this.width;
-        h = this.height;
-        obj = {};
+    _imagePreviewInstance.readFiles(element);
+  };
 
-        if (!isCorrectType) {
-          element.value = '';
+  ImagePreview.prototype.addEvent = function (element, events, callback) {
+    var event, eventCounter, eventLength, moderator;
 
-          if (typeof fFn !== 'undefined') {
-            obj.origin = {};
-            obj.uploaded = {};
-            obj.uploaded.file = element.files[0];
-            obj.origin.allowType = allowTypeArr;
-            obj.uploaded.allowType = type;
+    moderator = function (event) {
+      var e = event || window.event;
 
-            fFn(obj);
-          }
-
-          return false;
-        } else if ((width && width != w) || (height && height != h)) {
-          element.value = '';
-
-          if (typeof fFn !== 'undefined') {
-            obj.origin = {};
-            obj.uploaded = {};
-            obj.uploaded.file = element.files[0];
-            obj.origin.width = width;
-            obj.uploaded.width = w;
-            obj.origin.width = height;
-            obj.uploaded.width = h;
-
-            fFn(obj);
-          }
-
-          return false;
-        } else {
-          if (typeof sFn !== 'undefined') {
-            obj.uploaded = {};
-            obj.uploaded.file = element.files[0];
-            sFn(obj);
-          }
-
-          element.parentNode.querySelectorAll('img[data-get-preview]')[0].src = e.target.result;
-        }
-        console.log('Uploaded image\'s name is ' + name);
+      if (!e.target) {
+        e.target = e.srcElement;
       }
+
+      if (!e.preventDefault) {
+        e.preventDefault = function () {
+          e.returnValue = false;
+          e.defaultPrevented = true;
+        };
+      }
+
+      return callback.call(this, e);
     };
-    reader.readAsDataURL(element.files[0]);
-  }
-};
+
+    events = events.split(' ');
+    eventCounter = 0;
+    eventLength = events.length;
+
+    for (; eventCounter < eventLength; eventCounter++) {
+      event = events[eventCounter];
+
+      if (element.addEventListener) {
+        element.addEventListener(event, callback, false);
+      } else {
+        element.attachEvent('on' + event, moderator);
+      }
+    }
+  };
+
+  ImagePreview.prototype.readFiles = function (element) {
+    var reader = new FileReader();
+
+    if (reader) {
+      var files, readFile;
+
+      files = element.files;
+      readFile = function (index) {
+        var file, fileVolume, imageType, fileInfo;
+
+        if (index >= files.length) return;
+
+        file = files[index];
+        imageType = /^image.*$/;
+
+        fileInfo = {};
+
+        fileInfo.fileName = file.name;
+        fileInfo.fileType = file.type;
+        fileInfo.fileVolume = file.size;
+        fileInfo.errorType = null;
+        fileInfo.width = 0;
+        fileInfo.height = 0;
+        fileInfo.ratio = 0;
+        fileInfo.image = null;
+
+        if (!imageType.test(file.type)) {
+          _imagePreviewInstance.obj.wrongFiles.push(fileInfo);
+          _imagePreviewInstance.obj.notFittable = true;
+          fileInfo.errorType = 'type';
+          if (index === files.length - 1) {
+            element.value = null;
+            if (typeof _imagePreviewInstance.obj.failedFunc === 'function') _imagePreviewInstance.obj.failedFunc(_imagePreviewInstance.obj.rightFiles, _imagePreviewInstance.obj.wrongFiles);
+          }
+        }
+
+        reader.onload = function (e) {
+          var div, image;
+
+          div = document.createElement('div');
+          image = document.createElement('img');
+
+          image.src = e.target.result;
+          image.onload = function () {
+            var w, h, ratio, type;
+
+            w = this.width;
+            h = this.height;
+            ratio = Number((h / w).toFixed(2));
+            fileExtension = fileInfo.fileType.replace(/^image\//, '');
+
+            fileInfo.width = w;
+            fileInfo.height = h;
+            fileInfo.ratio = ratio;
+            fileInfo.image = image;
+
+            if (
+                   (_imagePreviewInstance.obj.validateType.includes('volume') && (_imagePreviewInstance.obj.volume && fileInfo.fileVolume > _imagePreviewInstance.obj.volume) && (fileInfo.errorType = 'volume')) ||
+                   (_imagePreviewInstance.obj.validateType.includes('ratio') && (_imagePreviewInstance.obj.ratio && ratio !== _imagePreviewInstance.obj.ratio) && (fileInfo.errorType = 'ratio')) ||
+                   (_imagePreviewInstance.obj.validateType.includes('size') && (_imagePreviewInstance.obj.width && _imagePreviewInstance.obj.height && (w !== _imagePreviewInstance.obj.width || h !== _imagePreviewInstance.obj.height)) && (fileInfo.errorType = 'size')) ||
+                   (_imagePreviewInstance.obj.validateType.includes('type') && (_imagePreviewInstance.obj.type && !_imagePreviewInstance.obj.acceptType.includes(fileExtension)) && (fileInfo.errorType = 'type'))
+            ) {
+              _imagePreviewInstance.obj.wrongFiles.push(fileInfo);
+              _imagePreviewInstance.obj.notFittable = true;
+            } else {
+              _imagePreviewInstance.obj.rightFiles.push(fileInfo);
+              if (_imagePreviewInstance.obj.useBins) _imagePreviewInstance.appendFile(element, index, false);
+            }
+
+            if (typeof _imagePreviewInstance.obj.renderFunc === 'function') {
+              _imagePreviewInstance.obj.renderFunc(image);
+            } else {
+              element.insertAdjacentElement('afterend', image);
+            }
+
+            if (index === files.length - 1) {
+              if (_imagePreviewInstance.obj.notFittable) {
+                element.value = null;
+                if (typeof _imagePreviewInstance.obj.failedFunc === 'function') _imagePreviewInstance.obj.failedFunc(_imagePreviewInstance.obj.rightFiles, _imagePreviewInstance.obj.wrongFiles);
+              } else {
+                if (typeof _imagePreviewInstance.obj.successFunc === 'function') {
+                  if (_imagePreviewInstance.obj.useBins) {
+                    _imagePreviewInstance.appendFile(element, index, true);
+                  } else {
+                    _imagePreviewInstance.obj.successFunc(_imagePreviewInstance.obj.rightFiles, _imagePreviewInstance.obj.wrongFiles);
+                  }
+                }
+              }
+            }
+
+            return;
+          };
+
+          readFile(index + 1);
+        };
+        reader.readAsDataURL(file);
+      };
+      readFile(0);
+    }
+  };
+
+  ImagePreview.prototype.appendFile = function (element, index, isLast) {
+    var reader, files, readFile;
+
+    reader = new FileReader();
+    files = element.files;
+
+    if (isLast) {
+      if (typeof _imagePreviewInstance.obj.successFunc === 'function') {
+        _imagePreviewInstance.obj.successFunc(_imagePreviewInstance.obj.rightFiles, _imagePreviewInstance.obj.wrongFiles, _imagePreviewInstance.obj.bins);
+      }
+    } else {
+      readFile = function (index) {
+        var file = files[index];
+
+        reader.onload = function (e) {
+          var bin = e.target.result;
+
+          _imagePreviewInstance.obj.bins.push(bin);
+        };
+
+        reader.readAsBinaryString(file);
+      };
+
+      readFile(index);
+    }
+  };
+
+  window.imagePreview = imagePreview;
+})();
+
